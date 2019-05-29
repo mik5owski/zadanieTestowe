0ece415dcf34551a18d2da33b9d9975d65792711
mik5owski <piatkowskimikolaj@gmail.com>
2019-05-29 15:13:21 +0200

    moved styling to external css file. switched div element for ul element

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-// let fetchUsers = () => {fetch('https://randomuser.me/api/?results=10')
-//        .then(r => r.json()).then(r => {
-//            console.log(r.results)
-//        })
-// }
-
 fetchUsers = async () => {
     const users = await fetch('https://randomuser.me/api/?results=10')
                         .then(r => r.json())
                         .then(r => r.results)
     return users
-}
-
-logUsers = async () => {
-    const users = await fetchUsers()
-    console.log(users)
 }
 
 capitalize = (word) => {
@@ -28,22 +17,23 @@ showUserNames = async () => {
         const wrapper = document.querySelector('.wrapper')
         const container = document.createElement('div')
         const photo = document.createElement('div')
-        const name = document.createElement('span')
-        const email = document.createElement('div')
+        const data = document.createElement('ul')
+        const name = document.createElement('li')
+        const email = document.createElement('li')
+
+        container.setAttribute('class', 'container')
+        photo.setAttribute('class', 'photo')
+        data.setAttribute('class', data)
 
         name.innerHTML = capitalize(user.name.first) + ' ' + capitalize(user.name.last)
         email.innerHTML = 'Email: ' + user.email
 
-        photo.style.width = '70px'
-        photo.style.height = '70px'
         photo.style.background = `url(${user.picture.medium})`
-        container.style.width = '300px'
-        container.style.border = '1px solid black'
-        container.style.marginBottom = '5px'
 
         container.appendChild(photo)
-        container.appendChild(name)
-        container.appendChild(email)
+        container.appendChild(data)
+        data.appendChild(name)
+        data.appendChild(email)
         wrapper.appendChild(container)
 
     })
@@ -51,4 +41,3 @@ showUserNames = async () => {
 }
 
 showUserNames()
-logUsers()
