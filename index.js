a0ad10bedd8ea291242ac6b5882b70c487a652b7
mik5owski <piatkowskimikolaj@gmail.com>
2019-05-29 14:23:47 +0200

    added fucntion to show user names and surnames

--- index.js
+++ index.js
@@ -17,4 +17,23 @@ logUsers = async () => {
 }
 
 
+showUserNames = async () => {
+    const users = await fetchUsers()
+
+    users.map(user => {
+
+        const wrapper = document.querySelector('.wrapper')
+        const container = document.createElement('div')
+        const name = document.createElement('span')
+
+        name.innerHTML = user.name.first + ' ' + user.name.last
+
+        container.appendChild(name)
+        wrapper.appendChild(container)
+
+    })
+
+}
+
+showUserNames()
 logUsers()
